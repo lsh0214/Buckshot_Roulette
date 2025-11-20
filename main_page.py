ebb0579f0fee886964e4555efb29d2b3b2686eb6
jooha2006 <134946078+jooha2006@users.noreply.github.com>
2025-11-20 20:50:22 +0900

    모든 요소 함수화
    진짜 힘들었어

--- main_page.py
+++ main_page.py
@@ -3,12 +3,12 @@ import pygame
 import sys
 import webbrowser
 
-GameLobby_img = "gameLobby.png" #주소 기입 방식 찾아봤음
+"""쓸 변수들 정리"""
+GAME_LOBBY_IMG = "gameLobby.png" #주소 기입 방식 찾아봤음
 WHITE = (192, 192, 192)
 L_BLACK = (95,95,95)
 BLACK = (0,0,0)
 FONT = "Fake Receipt.otf"
-BULLET = ''
 LOGO = pygame.image.load('Buckshot_logo.png')
 LOGO = pygame.transform.scale(LOGO, (950,750))
 KANGNAM_LOGO = pygame.image.load('KangnamUniversity.png')
@@ -18,16 +18,33 @@ MENU_BULLET_IMG_2 = pygame.image.load('menu_bullet_img2.png')
 SCROLL_SPEED = 15
 TIME = pygame.time.Clock()
 
-def get_font(size):
-    return pygame.font.Font(FONT, size) # 기본 폰트 사용
+"""
+초기 설정
+"""
+pygame.init()
 
-#커서 바뀌게 만들 함수들입니다.
+for m in get_monitors():
+    if m.is_primary:
+        screen_width, screen_height = m.width, m.height
+        break
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
+screen_height_half = screen_height/2
+screen_width_half = screen_width/2
+
+state = 'menu'
+
+
+#기본 폰트 함수
+def get_font(size):
+    return pygame.font.Font(FONT, size)
+
+#커서 모양 바꾸기 함수
 def mouse_cursor_hand():
     return pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
 def mouse_cursor_arrow():
     return pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
-
+#버튼 클래스
 class Button():
     """
     텍스트 버튼을 위한 재사용 가능한 클래스
@@ -101,111 +118,113 @@ class Button():
                 mouse_cursor_hand()
             else:
                 mouse_cursor_arrow()
+#스크롤 위치 세팅
+MENU_BULLET_IMG_1 = pygame.transform.scale(MENU_BULLET_IMG_1, (screen_width,screen_height))
+MENU_BULLET_IMG_2 = pygame.transform.scale(MENU_BULLET_IMG_2, (screen_width,screen_height))
+
+MENU_BULLET_IMG_1_y_pos = 0
+MENU_BULLET_IMG_2_y_pos = screen_height
+
+#버튼 세팅
+start_btn = Button(None, screen_width_half, screen_height_half + 350, "START", get_font(80), WHITE, L_BLACK)
+multi_btn = Button(None, screen_width_half, screen_height_half + 430, "MULTIPLAY", get_font(80), WHITE, L_BLACK)
+option_btn = Button(None, screen_width_half, screen_height_half + 510, "OPTIONS", get_font(80), WHITE, L_BLACK)
+credit_btn = Button(None, screen_width_half, screen_height_half + 590, "CREDITS", get_font(80), WHITE, L_BLACK)
+exit_btn = Button(None, screen_width_half, screen_height_half + 670, "EXIT", get_font(80), WHITE, L_BLACK)
+kangnam_link_btn = Button(KANGNAM_LOGO, screen_width - 200 + 100, screen_height - 200 + 100)
+
+menu_buttons = [start_btn, multi_btn, option_btn, credit_btn, exit_btn, kangnam_link_btn]
+
+credit_exit_btn = Button(None, screen_width_half, screen_height_half + 670, "EXIT", get_font(80), WHITE, L_BLACK)
+# 위 크래딧 버튼은 후에 또 바꿀 예정
+
+#총알 내려오는 거 홤수화
+def scroll_bullet():
+    global MENU_BULLET_IMG_1_y_pos, MENU_BULLET_IMG_2_y_pos
+    MENU_BULLET_IMG_1_y_pos +=SCROLL_SPEED
+    MENU_BULLET_IMG_2_y_pos +=SCROLL_SPEED
+
+    if MENU_BULLET_IMG_1_y_pos >=screen_height:
+        MENU_BULLET_IMG_1_y_pos = MENU_BULLET_IMG_2_y_pos - screen_height
+    if MENU_BULLET_IMG_2_y_pos >=screen_height:
+        MENU_BULLET_IMG_2_y_pos = MENU_BULLET_IMG_1_y_pos - screen_height
+
+#메뉴 블릿 함수
+def menu_blit(mouse_pos):
+    screen.fill(BLACK)
+    scroll_bullet()
+    
+    #버전 텍스트
+    txt = get_font(40)
+    version_txt = txt.render('0.0.1(Prototype)', True, L_BLACK)
+
+    #로고 이미지
+    LOGO_size_width = LOGO.get_rect().size[0]
+    LOGO_x_position = screen_width_half-(LOGO_size_width/2)
+
+    #blit 모음
+    screen.blit(LOGO, (LOGO_x_position, 250))
+    screen.blit(MENU_BULLET_IMG_1, (0, MENU_BULLET_IMG_1_y_pos))
+    screen.blit(MENU_BULLET_IMG_2, (0, MENU_BULLET_IMG_2_y_pos))
+    screen.blit(version_txt,(30, screen_height - 80))
+
+    for btn in menu_buttons:
+        btn.check_for_hover(mouse_pos)
+        btn.update(screen)
+
+#크래딧 블릿 함수
+def credit_blit(mouse_pos):
+    text_font = get_font(50)
+    credit_text = text_font.render("Credits", True, WHITE)
+    credit_rect = credit_text.get_rect(center=(screen_width_half, screen_height_half - 100))
+    screen.blit(credit_text, credit_rect)
+
+    credit_exit_btn.check_for_hover(mouse_pos)
+    credit_exit_btn.update(screen)
 
 # --- 메인 게임 루프 ---
-
 def main():
-    pygame.init()
-    global MENU_BULLET_IMG_1, MENU_BULLET_IMG_2
-
-    # 메인 모니터의 해상도 자동 감지
-    for m in get_monitors():
-        if m.is_primary:
-            screen_width = m.width
-            screen_height = m.height
-            break
-
-    screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
-    screen_height_half = screen_height/2
-    screen_width_half = screen_width/2
-
-    MENU_BULLET_IMG_1 = pygame.transform.scale(MENU_BULLET_IMG_1, (screen_width,screen_height))
-    MENU_BULLET_IMG_2 = pygame.transform.scale(MENU_BULLET_IMG_2, (screen_width,screen_height))
-    MENU_BULLET_IMG_1_y_pos = 0
-    MENU_BULLET_IMG_2_y_pos = screen_height
+    global state
 
     running = True
     while running:
-        
+        TIME.tick(60)
+        mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
-
-        Kangnam_size_width = KANGNAM_LOGO.get_rect().size[0]
-        Kangname_size_height = KANGNAM_LOGO.get_rect().size[1]
-        
-        kangnam_x_position = screen_width - (Kangnam_size_width) + 100
-        kangnam_y_position = screen_height - (Kangname_size_height) + 100   
-        
-        start_btn = Button(None,screen_width_half,screen_height_half + 350, "START", get_font(80), WHITE, L_BLACK)
-        multi_btn = Button(None,screen_width_half,screen_height_half + 430, "MULTIPLAY", get_font(80), WHITE, L_BLACK)
-        option_btn = Button(None,screen_width_half,screen_height_half + 510, "OPTIONS", get_font(80), WHITE, L_BLACK)
-        credit_btn = Button(None,screen_width_half,screen_height_half + 590, "CREDITS", get_font(80), WHITE, L_BLACK)
-        exit_btn = Button(None,screen_width_half,screen_height_half + 670, "EXIT", get_font(80), WHITE, L_BLACK)
-        kangnam_link_btn = Button(KANGNAM_LOGO, kangnam_x_position, kangnam_y_position)
-
-        buttons = [start_btn, multi_btn, option_btn, credit_btn, exit_btn, kangnam_link_btn]
-
-        while 1:
-            screen.fill(BLACK)
-            TIME.tick(60)
-
-            txt = get_font(40)
-            version_txt = txt.render('0.0.1(Prototype)', True, L_BLACK)
-            screen.blit(version_txt,[30, screen_height-80])
-
-            LOGO_size_width = LOGO.get_rect().size[0]
-            # LOGO_size_height = LOGO.get_rect().size[1]
-
-            MENU_BULLET_IMG_1_y_pos +=SCROLL_SPEED
-            MENU_BULLET_IMG_2_y_pos +=SCROLL_SPEED
-
-            if MENU_BULLET_IMG_1_y_pos >=screen_height:
-                MENU_BULLET_IMG_1_y_pos = MENU_BULLET_IMG_2_y_pos - screen_height
-            if MENU_BULLET_IMG_2_y_pos >=screen_height:
-                MENU_BULLET_IMG_2_y_pos = MENU_BULLET_IMG_1_y_pos - screen_height
-
-            screen.blit(MENU_BULLET_IMG_1, (0,MENU_BULLET_IMG_1_y_pos))
-            screen.blit(MENU_BULLET_IMG_2, (0,MENU_BULLET_IMG_2_y_pos))
-            screen.blit(version_txt,[30, screen_height-80])
-
-            
-                
-
-            LOGO_x_position = screen_width_half-(LOGO_size_width/2)
-            screen.blit(LOGO, (LOGO_x_position, 250))
-
-            mouse_pos = pygame.mouse.get_pos()
-            for i in buttons:
-                i.check_for_hover(mouse_pos)
-                i.update(screen)
-            
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if state == 'menu':
                     if start_btn.check_for_input(mouse_pos):
                         print("시작 버튼 눌림")
-                    if multi_btn.check_for_input(mouse_pos):
+                        # 게임 시작 로직 추가
+                    elif multi_btn.check_for_input(mouse_pos):
                         print("멀티 버튼 눌림")
-                    if option_btn.check_for_input(mouse_pos):
+                    elif option_btn.check_for_input(mouse_pos):
                         print("옵션 버튼 눌림")
-                    if credit_btn.check_for_input(mouse_pos):
+                    elif credit_btn.check_for_input(mouse_pos):
                         print("크레딧 버튼 눌림")
-                    if exit_btn.check_for_input(mouse_pos):
+                        state = "credit"
+                    elif exit_btn.check_for_input(mouse_pos):
                         print("탈출 버튼 눌림")
-                        pygame.quit()
-                        sys.exit()
-                    if kangnam_link_btn.check_for_input(mouse_pos):
+                        running = False
+                    elif kangnam_link_btn.check_for_input(mouse_pos):
                         webbrowser.open('https://web.kangnam.ac.kr/')
-            pygame.display.update()
-
-        pygame.display.flip()
+                elif state == "credit":
+                    if credit_exit_btn.check_for_input(mouse_pos):
+                        print("크레딧 탈출 버튼 눌림")
+                        state = "menu"
+        if state == 'menu':
+            menu_blit(mouse_pos)
+        elif state == 'credit':
+            credit_blit(mouse_pos)
+        pygame.display.update()
 
     pygame.quit()
+    sys.exit()
+
 
 if __name__ == '__main__':
     main()
