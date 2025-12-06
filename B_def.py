8ab0c8495c1bfb2f05a6d6c4ce9f86575fd2e814
lsh0214 <netstat3476@naver.com>
2025-12-07 00:43:16 +0900

    ai턴 4단계 중반까지 도입 및 대포폰 리턴값 영어로 변경 및 변경에 따른 실탄 공포탄을 다루는 내부 이름 모두 영문화

--- B_def.py
+++ B_def.py
@@ -3,7 +3,7 @@ import lsj_r
 # 처음 세팅해주는 클래스 만들기!!
 
 class Action:
-    bullet=[]#'실탄' or '공포탄' 
+    bullet=[]#'live bullet' or '공포탄' 
 
     def __init__(self,hp_max):
         self.hp_max=hp_max
@@ -34,20 +34,20 @@ class Action:
     def inven_in(self,item,inven_index):
         self.inven[inven_index]=item
     
-    def Shotgun(self,Shotgun_Bool,target): #각각 실탄일시 hp다운 시스템 및 총알은 index -1빠지도록
+    def Shotgun(self,Shotgun_Bool,target): #각각 live bullet일시 hp다운 시스템 및 총알은 index -1빠지도록
         if Shotgun_Bool==0:
-            if self.bullet[-1]=="실탄":
+            if self.bullet[-1]=="live bullet":
                 self.hp -= self.shotgun_hp
             else:
                 self.turn_bool[0]=0
         else:
-            if self.bullet[-1]== "실탄":
+            if self.bullet[-1]== "live bullet":
                 target.hp -= self.shotgun_hp
 
         if self.shotgun_hp==2:
             self.shotgun_hp=1
-            return "톱"+"실탄" if self.bullet.pop() == "실탄" else "공포탄"
-        return "실탄" if self.bullet.pop() == "실탄" else "공포탄"
+            return "톱"+"live bullet" if self.bullet.pop() == "live bullet" else "dummy bullet"
+        return "live bullet" if self.bullet.pop() == "live bullet" else "dummy bullet"
     
     # 모든 아이템 함수는 선택한 아이템 목록값을 0으로 변경합니다.
     def Handcuffs(self,inven_index,ad=0): #턴 여부를 확인하는 리스트 [1]값을 1으로 변경합니다.
@@ -81,19 +81,19 @@ class Action:
         if ad ==0:
             self.inven[inven_index]=0
         if len(self.bullet) == 1:
-            return "안타깝게... 됐군..."
+            return "Unfortunately... that\'s it..."
         else:
             r_index=lsj_r.rint(0,len(self.bullet)-2)
             r_num=len(self.bullet)-r_index
-            return str(r_num)+"번째 탄... "+str(self.bullet[r_index])+"이다..."
+            return str(r_num)+"th bullet... "+str(self.bullet[r_index])+"..."
 
     def Inverter(self,inven_index,ad=0):#극성을 전환시켜준다.
         if ad ==0:
             self.inven[inven_index]=0
-        if self.bullet[-1]=="실탄":
-            self.bullet[-1]="공포탄"
+        if self.bullet[-1]=="live bullet":
+            self.bullet[-1]="dummy bullet"
         else:
-            self.bullet[-1]="실탄"
+            self.bullet[-1]="live bullet"
 
     def Expired_Medicine(self,inven_index,ad=0):#각 50%확률로 2칸 올리기 or 한칸 소모
         if 5 < lsj_r.rint(1,10):
@@ -115,7 +115,8 @@ class Action:
         target.inven[target_index]=0
         return result
 #메인에서 ai용 카운트하는 변수 만들어서 사용
-#ai_count=[실탄 수,공포탄 수]
+#ai_count=[live bullet 수,공포탄 수]
+#ing_bullet = 알수없음 0, 실탄 1 공포탄 2
 #유저와 ai 객체 명 각각 user, ai
 def ai_turn(ai_class,ai_count,target,ing_bullet=0):
     ret_list=[]
@@ -171,6 +172,12 @@ def ai_turn(ai_class,ai_count,target,ing_bullet=0):
                 ret_list.append(ai_class.Adrenaline(inven_index,target,target_inven_index))
                 end_bool=1
         #4단계 정보수집
+        if ing_bullet == 0:
+            if "Burner_Phone" in ai_class.inven:
+                inven_index=ai_class.inven.index("Burner_Phone")
+                ret_list.append("대"+str(inven_index))
+                ret_list.append(ai_class.Burner_Phone(inven_index))
+                end_bool=1
         
         
         
